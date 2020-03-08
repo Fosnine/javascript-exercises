4c34986883a44ae58f4fc56a413b97c57e6bef68
Jani Keustermans <janikeus@gmail.com>
2020-03-08 18:55:00 +0100

    repeatString function added_ test ok

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, n) { 
+        let i;
+        let stringresult = "";
+        if (n<0) {
+            return stringresult = "ERROR";
+        } else {
+            for (i=0; i<n; i++) {
+                stringresult += "hey";
+            }
+            return stringresult;
+        }
+    }
 
-}
 
 module.exports = repeatString
