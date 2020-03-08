27970fb65307f637d6df6de3c4e256bb53680cfd
Jani Keustermans <janikeus@gmail.com>
2020-03-08 19:46:05 +0100

    added stringreverse function. test ok

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let strArr = Array.from(string);
+    let newString ="";
+    for (let i = 1; i<=strArr.length ; i++) {
+        newString += strArr[string.length - i];
+    }
+    return newString;
 }
 
 module.exports = reverseString
