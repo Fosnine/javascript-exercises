2b7e4efd7fc712a964904582787c0f40d7f2ae62
Jani Keustermans <janikeus@gmail.com>
2020-03-08 21:00:18 +0100

    added removefromarray function

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...arguments) {
+    let array = arguments[0];
+    let newArray = [];
+    array.forEach((item) => {
+        if (!(arguments.includes(item))) {
+            newArray.push(item);
+        } 
+    });
+    return newArray;
 }
 
 module.exports = removeFromArray
