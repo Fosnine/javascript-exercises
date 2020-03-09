b75b97211ac71a0352ba31c096bc0b7c46935451
Jani Keustermans <janikeus@gmail.com>
2020-03-09 18:00:07 +0100

    added tempConversion functions_test ok

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let tempC;
+  tempC = (tempF - 32) * (5/9);
+  return +(tempC.toFixed(1));
 }
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let tempF;
+  tempF = tempC * (9/5) + 32;
+  return +(tempF.toFixed(1));
 }
 
 module.exports = {
