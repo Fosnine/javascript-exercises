a5667873f34c08f95535717d71a035e3268ab60e
Jani Keustermans <janikeus@gmail.com>
2020-03-24 18:38:05 +0100

    added working calc functions

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,41 @@
-function add () {
-	
+function add (a, b) {
+	return a+b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a-b;
 }
 
-function sum () {
-	
+function sum (a) {
+	let sum = 0;
+	for ( let i=0; i < a.length; i++) {
+		sum += +a[i];
+	}
+	return sum;
 }
 
-function multiply () {
-	
+function multiply (a) {
+	let product = 1;
+	for ( let i=0; i < a.length; i++) {
+		product *= +a[i];
+	}
+	return product;
 }
 
-function power() {
-	
+function power(a, b) {
+	return a**b;
 }
 
-function factorial() {
-	
+function factorial(a) {
+	let factorial = 1;
+	if (a==0) {
+		return 1;
+	} else {
+		for (let i = 1; i <= a; i++) {
+		factorial *= i;
+		}
+		return factorial;
+	}
 }
 
 module.exports = {
