c4ff771a9e33978b744315f803db8d277de82ad5
Jani Keustermans <janikeus@gmail.com>
2020-03-31 17:03:14 +0200

    coded palindrome checker function, tests ok

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function() {
+const palindromes = function( word ) {
+    let palindrome = [];
+    let word2 = removePunctuation( word );
+    for (i=word2.length-1; i>=0; i--) {   
+         palindrome += word2.slice(i, i + 1);
+    }
+    return (word2.toLowerCase() === palindrome.toLowerCase()) ? true : false;
+}
+const removePunctuation = function( word ) {
+    let word2 = [];
+    for (i=0; i<=word.length - 1; i++) {
+        if (word.slice(i, i+1) != "." && word.slice(i, i+1) != "!" && word.slice(i, i+1) != "?" && word.slice(i, i+1) != " " && word.slice(i, i+1) != "," ){
+        word2 += word.slice(i, i+1);
+        }
+    }
+    return word2;
 
 }
 
